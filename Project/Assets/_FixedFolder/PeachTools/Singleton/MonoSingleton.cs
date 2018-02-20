1d71bd311524dfd0aca32fb7a9de923adf879e83
杨磊 <905527992@qq.com>
2018-02-20 14:34:55 +0800

    添加单例基类

---
+++ Project/Assets/_FixedFolder/PeachTools/Singleton/MonoSingleton.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T> {
+
+	protected static T _instance = null;
+
+	public static T Instance {
+		get {
+			if (_instance == null) {
+				_instance = FindObjectOfType<T> ();
+
+				if (FindObjectsOfType<T> ().Length > 1) {
+					Debug.LogErrorFormat ("{0}实例超过1个!", _instance);
+					return _instance;
+				}
+
+				if (_instance == null) {
+					string instanceName = typeof (T).Name;
+					Debug.LogFormat ("实例名称:{0}", instanceName);
+					GameObject instanceGO = GameObject.Find (instanceName);
+
+					if (instanceGO == null)
+						instanceGO = new GameObject (instanceName);
+
+					_instance = instanceGO.AddComponent<T> ();
+					DontDestroyOnLoad (instanceGO); //保证实例不会被释放
+					Debug.LogFormat ("添加一个新的单例:{0}", instanceName);
+					return _instance;
+				} else {
+					Debug.LogWarningFormat ("已经存在这个实例:{0}", _instance.name);
+				}
+
+				GameObject go = new GameObject (typeof (T).Name);
+				_instance = go.AddComponent<T> ();
+				DontDestroyOnLoad (go);
+				Debug.LogFormat ("新建实例:{0}", go.name);
+			}
+			return _instance;
+		}
+	}
+
+	private void Awake () {
+		if (_instance == null) {
+			_instance = gameObject.GetComponent<T> ();
+
+			if (_instance != null)
+				DontDestroyOnLoad (gameObject);
+		}
+		OnAwake ();
+	}
+
+	private void OnDestroy () {
+		_instance = null;
+	}
+
+	public virtual void OnAwake () { }
+}
---
+++ Project/Assets/_FixedFolder/PeachTools/Singleton/Singleton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Singleton<T> where T : Singleton<T> {
+	protected static T _instance = null;
+	protected Singleton () { }
+	public static T Instance {
+		get {
+			if (_instance == null) {
+				_instance = System.Activator.CreateInstance (typeof (T), true) as T;
+				if (_instance == null)
+					throw new System.Exception ("创建单例失败");
+			}
+			return _instance;
+		}
+	}
+}
