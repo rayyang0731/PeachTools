810b9d719d6ab73cca1f970267e13a23b95d9070
杨磊 <905527992@qq.com>
2018-03-01 17:23:18 +0800

    添加游戏帧率显示,添加运行中控制台显示

---
+++ Project/Assets/_FixedFolder/PeachTools/Debug/GameFPS.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GameFPS : BaseMonoBehaviour, IUpdate {
+    public float updateInterval = 0.5f;
+
+    private float lastInterval;
+    private float frames = 0;
+    private float fps;
+
+    private Color color;
+
+    private static GUIStyle _normalLabelStyle = null;
+
+    public static GUIStyle NormalLabelStyle {
+        get {
+            if (_normalLabelStyle == null) {
+                _normalLabelStyle = new GUIStyle (GUI.skin.label);
+                _normalLabelStyle.fontSize = 40;
+                _normalLabelStyle.fontStyle = FontStyle.Bold;
+            }
+            return _normalLabelStyle;
+        }
+    }
+
+    private void OnGUI () {
+        GUI.color = color;
+        GUI.Label (
+            new Rect (5, 5, 300, 50),
+            string.Format ("FPS: {0}", fps.ToString ("f2")), NormalLabelStyle);
+    }
+
+    private void Start () {
+        lastInterval = Time.realtimeSinceStartup;
+        frames = 0;
+    }
+
+    public void _Update () {
+        ++frames;
+        var timeNow = Time.realtimeSinceStartup;
+        if (timeNow > lastInterval + updateInterval) {
+            fps = frames / (timeNow - lastInterval);
+            frames = 0;
+            lastInterval = timeNow;
+        }
+        if (fps < 25)
+            color = Color.yellow;
+        else if (fps < 15)
+            color = Color.red;
+        else
+            color = Color.green;
+    }
+}
---
+++ Project/Assets/_FixedFolder/PeachTools/Debug/UnityLog.cs
@@ -0,0 +1,175 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>  
+/// A console to display Unity's debug logs in-game.  
+/// </summary>  
+public class UnityLog : BaseMonoBehaviour {
+    struct Log {
+        public string message;
+        public string stackTrace;
+        public LogType type;
+    }
+
+    #region Inspector Settings  
+
+    /// <summary>  
+    /// Whether to only keep a certain number of logs.  
+    ///  
+    /// Setting this can be helpful if memory usage is a concern.  
+    /// </summary>  
+    public bool restrictLogCount = false;
+
+    /// <summary>  
+    /// Number of logs to keep before removing old ones.  
+    /// </summary>  
+    public int maxLogs = 1000;
+
+    #endregion
+
+    readonly List<Log> logs = new List<Log> ();
+    public Vector2 scrollPosition;
+    bool visible;
+    bool collapse;
+    bool stackTrace;
+
+    // Visual elements:  
+
+    static readonly Dictionary<LogType, Color> logTypeColors = new Dictionary<LogType, Color> { { LogType.Assert, Color.white },
+        { LogType.Error, Color.red },
+        { LogType.Exception, Color.red },
+        { LogType.Log, Color.white },
+        { LogType.Warning, Color.yellow },
+    };
+
+    const string windowTitle = "Console";
+    const string StackTraceFormat = "<<<[ LOG ]<<<<<<<<<<<<\r\n{0}\r\n----- Stack Trace ------:\r\n{1}>>>>>>>>>>>>>>>>>>>>";
+    const string LogFormat = "[ LOG ]: {0}";
+    const int margin = 5;
+    static readonly GUIContent clearLabel = new GUIContent ("Clear", "Clear the contents of the console.");
+    static readonly GUIContent closeLabel = new GUIContent ("Close", "Close the console.");
+    static readonly GUIContent collapseLabel = new GUIContent ("Collapse", "Hide repeated messages.");
+    static readonly GUIContent traceLabel = new GUIContent ("StackTrace", "Open stack trace message.");
+    Rect windowRect = new Rect (margin, margin, Screen.width - (margin * 2), Screen.height - (margin * 2));
+    Rect buttonRect = new Rect (Screen.width / 2f - 50f, 0f, 100f, 40f);
+
+    void Start () {
+        DontDestroyOnLoad (this.gameObject);
+    }
+
+    void OnEnable () {
+        Application.logMessageReceived += HandleLog;
+    }
+
+    void OnDisable () {
+        Application.logMessageReceived -= HandleLog;
+    }
+
+    void OnGUI () {
+        if (!visible) {
+            if (GUI.Button (buttonRect, windowTitle)) {
+                visible = true;
+            }
+            return;
+        }
+
+        windowRect = GUILayout.Window (123456, windowRect, DrawConsoleWindow, windowTitle);
+    }
+
+    /// <summary>  
+    /// Displays a window that lists the recorded logs.  
+    /// </summary>  
+    /// <param name="windowID">Window ID.</param>  
+    void DrawConsoleWindow (int windowID) {
+        if (GUILayout.Button (closeLabel, GUILayout.Height (100)))
+            visible = false;
+
+        DrawLogsList ();
+        DrawToolbar ();
+    }
+
+    /// <summary>  
+    /// Displays a scrollable list of logs.  
+    /// </summary>  
+    void DrawLogsList () {
+        GUIStyle font = new GUIStyle (GUI.skin.label);
+        font.fontSize = 30;
+
+        scrollPosition = GUILayout.BeginScrollView (scrollPosition);
+
+        // Iterate through the recorded logs.  
+        for (var i = 0; i < logs.Count; i++) {
+            var log = logs[i];
+
+            // Combine identical messages if collapse option is chosen.  
+            if (collapse && i > 0) {
+                var previousMessage = logs[i - 1].message;
+
+                if (log.message == previousMessage) {
+                    continue;
+                }
+            }
+
+            GUI.contentColor = logTypeColors[log.type];
+
+            if (stackTrace) {
+                GUILayout.Label (string.Format (StackTraceFormat, log.message, log.stackTrace), font);
+            } else {
+                GUILayout.Label (string.Format (LogFormat, log.message), font);
+            }
+        }
+
+        GUILayout.EndScrollView ();
+
+        // Ensure GUI colour is reset before drawing other components.  
+        GUI.contentColor = Color.white;
+    }
+
+    /// <summary>  
+    /// Displays options for filtering and changing the logs list.  
+    /// </summary>  
+    void DrawToolbar () {
+        GUILayout.BeginHorizontal ();
+
+        collapse = GUILayout.Toggle (collapse, collapseLabel, GUILayout.ExpandWidth (false));
+        GUILayout.Space (100);
+        if (GUILayout.Button (clearLabel)) logs.Clear ();
+        GUILayout.Space (100);
+        stackTrace = GUILayout.Toggle (stackTrace, traceLabel, GUILayout.ExpandWidth (false));
+
+        GUILayout.EndHorizontal ();
+    }
+
+    /// <summary>  
+    /// Records a log from the log callback.  
+    /// </summary>  
+    /// <param name="message">Message.</param>  
+    /// <param name="stackTrace">Trace of where the message came from.</param>  
+    /// <param name="type">Type of message (error, exception, warning, assert).</param>  
+    void HandleLog (string message, string stackTrace, LogType type) {
+        logs.Add (new Log {
+            message = message,
+                stackTrace = stackTrace,
+                type = type,
+        });
+
+        TrimExcessLogs ();
+    }
+
+    /// <summary>  
+    /// Removes old logs that exceed the maximum number allowed.  
+    /// </summary>  
+    void TrimExcessLogs () {
+        if (!restrictLogCount) {
+            return;
+        }
+
+        var amountToRemove = Mathf.Max (logs.Count - maxLogs, 0);
+
+        if (amountToRemove == 0) {
+            return;
+        }
+
+        logs.RemoveRange (0, amountToRemove);
+    }
+}
