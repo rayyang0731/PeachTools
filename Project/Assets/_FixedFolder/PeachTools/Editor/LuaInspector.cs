279092c93d8d219c65f83c632c548f1367bbab96
LONGTU\yanglei <rayyang@longtugame.com>
2018-04-20 17:32:02 +0800

    添加在Unity的Inspector面板中查看Lua文件的编辑器扩展

---
+++ Project/Assets/_FixedFolder/PeachTools/Editor/LuaInspector.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor (typeof (UnityEditor.DefaultAsset))]
+public class LuaInspector : Editor {
+	public override void OnInspectorGUI () {
+		string path = AssetDatabase.GetAssetPath (target);
+		if (path.EndsWith (".lua")) {
+			GUI.enabled = true;
+			GUI.backgroundColor = new Color (63, 63, 63);
+
+			string lua = File.ReadAllText (path);
+
+			GUILayout.TextArea (lua);
+		}
+	}
+}
